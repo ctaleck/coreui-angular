93328efb63c4214d7f1b1f4a66be6396a6e1c80c
Taleck Christopher <Christopher.Taleck@srpnet.com>
2019-01-16 16:16:47 -0700

    Adds layout sample app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  AppAsideModule,
+  AppBreadcrumbModule,
+  AppHeaderModule,
+  AppFooterModule,
+  AppSidebarModule
+} from 'coreui-angular';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar'; // coreui dep
+import { TabsModule } from 'ngx-bootstrap/tabs'; // coreui dep
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppAsideModule,
+    AppBreadcrumbModule.forRoot(),
+    AppFooterModule,
+    AppHeaderModule,
+    AppSidebarModule,
+    PerfectScrollbarModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
